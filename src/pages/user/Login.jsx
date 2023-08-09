b0ed6e87faedeea889f94258373b516255f45a59
marquezin <gabrielmarques2003@gmail.comgit config --global user.email gabrielmarques2003@gmail.comgit config --global user.name marquezzinclear>
2023-08-09 12:32:17 -0300

    login

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,23 +1,48 @@
 import { useState } from "react";
+import { api } from "../../utils/api";
+import { useNavigate } from "react-router-dom";
+
+
 
 export function Login() {
+    const navigate=useNavigate()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [user, setUser] = useState(null)
+
+    const handlesubmit = (ev) => {
+        ev.preventDefault()
+        if(!email || !password){
+            alert("preencha os campos")
+            return
+        }
+        api.post("users/login",{email,password})
+        .then((res)=>{
+            console.log(res.data)
+            if (res.data.is_admin) {
+                alert("Successfully logged in as admin");
+                navigate('/categories'); // Redirecionar apenas se for administrador.
+            } else {
+                alert("Successfully logged in");
+            }
+        })
+        .catch((err) => {
+            alert("Login error ");
+            });
+    }
 
     return (
         <section className="flex flex-col">
             <h1 className="self-center m-6 text-4xl">login</h1>
-            <form className="flex flex-col items-center" onSubmit={(e) => {
-                e.preventDefault();
-            }}>
-                <input type="email" placeholder="email" value={email} onChange={(e) => {
-                    setEmail(e.target.value);
+            <form className="flex flex-col items-center" onSubmit={handlesubmit}>
+                <input type="email" placeholder="email" value={email} onChange={(ev) => {
+                    setEmail(ev.target.value);
                 }} className="outline-none bg-white rounded my-1 px-1 text-black text-xl h-8 w-80"></input>
 
-                <input type="password" placeholder="password" value={password} onChange={(e) => {
-                    setPassword(e.target.value);
+                <input type="password" placeholder="password" value={password} onChange={(ev) => {
+                    setPassword(ev.target.value);
                 }}  className="outline-none bg-white rounded my-1 px-1 text-black text-xl h-8 w-80"></input>
-                
+
                 <button type="submit" className="my-2 px-4 py-1 rounded bg-purple-700 text-xl">submit</button>
             </form>
         </section>
