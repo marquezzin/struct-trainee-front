1353b1bf5d7cba95d47c8e0e59b87126bc0c4626
leopcortes <leo.pereira.cortes@gmail.com>
2023-08-09 23:49:11 -0300

    style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export function Navbar() {
     return (
-        <nav className="h-16 bg-black text-black flex items-center justify-center text-xl">
+        <nav className="h-16 bg-green-800 text-black flex items-center justify-center text-xl">
             <Link to="/" className="px-4 py-1 mx-8 rounded bg-white text-3xl">HOME</Link>
             <Link to="/user/login" className="px-4 py-1 mx-8 rounded bg-white text-3xl">LOGIN</Link>
             <Link to="/user/signup" className="px-4 py-1 mx-8 rounded bg-white text-3xl">SIGNUP</Link>
