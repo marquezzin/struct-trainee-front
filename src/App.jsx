bad2210c88a3919b63e95c835b61225cf13650a9
leopcortes <leo.pereira.cortes@gmail.com>
2023-08-11 15:22:03 -0300

    organizing code

--- src/App.jsx
+++ src/App.jsx
@@ -13,57 +13,57 @@ import { Signup } from "./pages/user/Singup.jsx";
 import { UserContextProvider } from "./utils/UserContext.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root/>,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path: "/",
-        element: <Home/>
-      },
-      {
-        path: "createpost",
-        element: <CreatePost/>,
-      },
-      {
-        path: "post/:id",
-        element: <PostPage/>
-      },
-      {
-        path: "user/profile",
-        element: <Profile/>
-      },
-      {
-        path: "/user/login",
-        element: <Login/>
-      },
-      {
-        path: "/user/signup",
-        element: <Signup/>
-      },
-      {
-        path: "categories",
-        element: <AdminIndexCategoriesPage/>,
-      },
-      {
-        path: "categories/create",
-        element: <AdminCreateCategoriesPage/>,
-      },
-      {
-        path: "categories/update/:id",
-        element: <AdminEditCategoriesPage/>,
-      },
-    ]
-  },
+	{
+		path: "/",
+		element: <Root/>,
+		errorElement: <ErrorPage/>,
+		children: [
+			{
+				path: "/",
+				element: <Home/>
+			},
+			{
+				path: "createpost",
+				element: <CreatePost/>,
+			},
+			{
+				path: "post/:id",
+				element: <PostPage/>
+			},
+			{
+				path: "user/profile",
+				element: <Profile/>
+			},
+			{
+				path: "/user/login",
+				element: <Login/>
+			},
+			{
+				path: "/user/signup",
+				element: <Signup/>
+			},
+			{
+				path: "categories",
+				element: <AdminIndexCategoriesPage/>,
+			},
+			{
+				path: "categories/create",
+				element: <AdminCreateCategoriesPage/>,
+			},
+			{
+				path: "categories/update/:id",
+				element: <AdminEditCategoriesPage/>,
+			},
+		]
+	},
 ])
 
 function App() {
-  return (
-    <UserContextProvider>
-      <RouterProvider router={router}/>
-    </UserContextProvider>
-  )
+	return (
+		<UserContextProvider>
+			<RouterProvider router={router}/>
+		</UserContextProvider>
+	)
 }
 
 export default App
